b83d39958e55921a8a180419067970534c780155
srinivasreddy2108 <srinivasreddyn2108@gmail.com>
2026-09-08 12:56:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int digit1=0;
-        int m=n;
-        while(m){
-            digit1++;
-            m/=10;
-        }
-        if(digit1<=3){
+        if(n<1000){
             return 0;
         }
-
         int count=0;
         for(int i=1000;i<=n;i++){
-            int digit2=0;
-            int store=i;
-            while(store){
-                digit2++;
-                store/=10;
-            }
-            while(digit2){
-                count++;
-                digit2/=1000;
-            }
-            
+            count++;
         }
         return count;
     }
